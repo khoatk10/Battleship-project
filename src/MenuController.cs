61c20802fc87180566c9cb10cdceee227330433a
nt9517398 <101039526@student.swin.edu.au>
2018-10-12 00:43:25 +1100

    fixed code errors in menucontrollers.cs file

--- src/MenuController.cs
+++ src/MenuController.cs
@@ -1,322 +1,193 @@
-
-using Microsoft.VisualBasic;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-//using System.Data;
-using System.Diagnostics;
 using SwinGameSDK;
+using System;
 
-/// <summary>
-/// The menu controller handles the drawing and user interactions
-/// from the menus in the game. These include the main menu, game
-/// menu and the settings m,enu.
-/// </summary>
-
-static class MenuController
+// '' <summary>
+// '' The DeploymentController controls the players actions
+// '' during the deployment phase.
+// '' </summary>
+public static class DeploymentController
 {
+    // ships positioning support
+    // ships and text drawing support
+    private const int SHIPS_TOP = 98;
+    private const int SHIPS_LEFT = 20;
+    private const int SHIPS_HEIGHT = 90;
+    private const int SHIPS_WIDTH = 300;
+    private const int TOP_BUTTONS_TOP = 72;
+    private const int TOP_BUTTONS_HEIGHT = 46;
+    private const int PLAY_BUTTON_LEFT = 693;
+    private const int PLAY_BUTTON_WIDTH = 80;
+    private const int UP_DOWN_BUTTON_LEFT = 410;
+    private const int LEFT_RIGHT_BUTTON_LEFT = 350;
+    private const int RANDOM_BUTTON_LEFT = 547;
+    private const int RANDOM_BUTTON_WIDTH = 51;
+    private const int DIR_BUTTONS_WIDTH = 47;
+    private const int TEXT_OFFSET = 5;
+    private static Direction _currentDirection;
+    private static ShipName _selectedShip = ShipName.Tug;
 
-	/// <summary>
-	/// The menu structure for the game.
-	/// </summary>
-	/// <remarks>
-	/// These are the text captions for the menu items.
-	/// </remarks>
-	private static readonly string[][] _menuStructure = {
-		new string[] {
-			"PLAY",
-			"SETUP",
-			"SCORES",
-			"QUIT"
-		},
-		new string[] {
-			"RETURN",
-			"SURRENDER",
-			"QUIT"
-		},
-		new string[] {
-			"EASY",
-			"MEDIUM",
-			"HARD"
-		}
+    // '' <summary>
+    // '' Handles user input for the Deployment phase of the game.
+    // '' </summary>
+    // '' <remarks>
+    // '' Involves selecting the ships, deloying ships, changing the direction
+    // '' of the ships to add, randomising deployment, end then ending
+    // '' deployment
+    // '' </remarks>
+    public static void HandleDeploymentInput()
+    {
+        // Show the game menu when pressing "ESC"
+        if (SwinGame.KeyTyped(KeyCode.EscapeKey))
+        {
+            GameController.AddNewState(GameState.ViewingGameMenu);
+        }
 
-	};
-	private const int MENU_TOP = 575;
-	private const int MENU_LEFT = 30;
-	private const int MENU_GAP = 0;
-	private const int BUTTON_WIDTH = 75;
-	private const int BUTTON_HEIGHT = 15;
-	private const int BUTTON_SEP = BUTTON_WIDTH + MENU_GAP;
+        if (SwinGame.KeyTyped(KeyCode.UpKey) || SwinGame.KeyTyped(KeyCode.DownKey))
+        {
+            _currentDirection = Direction.UpDown;
 
-	private const int TEXT_OFFSET = 0;
-	private const int MAIN_MENU = 0;
-	private const int GAME_MENU = 1;
 
-	private const int SETUP_MENU = 2;
-	private const int MAIN_MENU_PLAY_BUTTON = 0;
-	private const int MAIN_MENU_SETUP_BUTTON = 1;
-	private const int MAIN_MENU_TOP_SCORES_BUTTON = 2;
+        }
 
-	private const int MAIN_MENU_QUIT_BUTTON = 3;
-	private const int SETUP_MENU_EASY_BUTTON = 0;
-	private const int SETUP_MENU_MEDIUM_BUTTON = 1;
-	private const int SETUP_MENU_HARD_BUTTON = 2;
+        if (SwinGame.KeyTyped(KeyCode.LeftKey) || SwinGame.KeyTyped(KeyCode.RightKey))
+        {
+            _currentDirection = Direction.LeftRight;
 
-	private const int SETUP_MENU_EXIT_BUTTON = 3;
-	private const int GAME_MENU_RETURN_BUTTON = 0;
-	private const int GAME_MENU_SURRENDER_BUTTON = 1;
 
-	private const int GAME_MENU_QUIT_BUTTON = 2;
-	private static readonly Color MENU_COLOR = SwinGame.RGBAColor(2, 167, 252, 255);
+        }
+        // Deply the ships randomly
+        if (SwinGame.KeyTyped(KeyCode.RKey))
+        {
+            GameController.HumanPlayer.RandomizeDeployment();
+        }
 
-	private static readonly Color HIGHLIGHT_COLOR = SwinGame.RGBAColor(1, 57, 86, 255);
-	/// <summary>
-	/// Handles the processing of user input when the main menu is showing
-	/// </summary>
-	public static void HandleMainMenuInput()
-	{
-		HandleMenuInput(MAIN_MENU, 0, 0);
-	}
+        if (SwinGame.MouseClicked(MouseButton.LeftButton))
+        {
+            ShipName selected = GetShipMouseIsOver();
+            if ((selected != ShipName.None))
+            {
+                _selectedShip = selected;
+            }
+            else
+            {
+                DoDeployClick();
+            }
 
-	/// <summary>
-	/// Handles the processing of user input when the main menu is showing
-	/// </summary>
-	public static void HandleSetupMenuInput()
-	{
-		bool handled = false;
-		handled = HandleMenuInput(SETUP_MENU, 1, 1);
+            if ((GameController.HumanPlayer.ReadyToDeploy && UtilityFunctions.IsMouseInRectangle(PLAY_BUTTON_LEFT, TOP_BUTTONS_TOP, PLAY_BUTTON_WIDTH, TOP_BUTTONS_HEIGHT)))
+            {
+                GameController.EndDeployment();
+            }
+            else if (UtilityFunctions.IsMouseInRectangle(UP_DOWN_BUTTON_LEFT, TOP_BUTTONS_TOP, DIR_BUTTONS_WIDTH, TOP_BUTTONS_HEIGHT))
+            {
+                _currentDirection = Direction.UpDown;
 
-		if (!handled) {
-			HandleMenuInput(MAIN_MENU, 0, 0);
-		}
-	}
+            }
+            else if (UtilityFunctions.IsMouseInRectangle(LEFT_RIGHT_BUTTON_LEFT, TOP_BUTTONS_TOP, DIR_BUTTONS_WIDTH, TOP_BUTTONS_HEIGHT))
+            {
+                _currentDirection = Direction.LeftRight;
 
-	/// <summary>
-	/// Handle input in the game menu.
-	/// </summary>
-	/// <remarks>
-	/// Player can return to the game, surrender, or quit entirely
-	/// </remarks>
-	public static void HandleGameMenuInput()
-	{
-		HandleMenuInput(GAME_MENU, 0, 0);
-	}
+            }
+            else if (UtilityFunctions.IsMouseInRectangle(RANDOM_BUTTON_LEFT, TOP_BUTTONS_TOP, RANDOM_BUTTON_WIDTH, TOP_BUTTONS_HEIGHT))
+            {
+                GameController.HumanPlayer.RandomizeDeployment();
+            }
+        }
+    }
 
-	/// <summary>
-	/// Handles input for the specified menu.
-	/// </summary>
-	/// <param name="menu">the identifier of the menu being processed</param>
-	/// <param name="level">the vertical level of the menu</param>
-	/// <param name="xOffset">the xoffset of the menu</param>
-	/// <returns>false if a clicked missed the buttons. This can be used to check prior menus.</returns>
-	private static bool HandleMenuInput(int menu, int level, int xOffset)
-	{
-		if (SwinGame.KeyTyped(KeyCode.vk_ESCAPE)) {
-			GameController.EndCurrentState();
-			return true;
-		}
+    // '' <summary>
+    // '' The user has clicked somewhere on the screen, check if its is a deployment and deploy
+    // '' the current ship if that is the case.
+    // '' </summary>
+    // '' <remarks>
+    // '' If the click is in the grid it deploys to the selected location with the indicated direction
+    // '' </remarks>
+    private static void DoDeployClick()
+    {
+        // Calculate the row/col clicked
+        Point2D mouse = SwinGame.MousePosition();
+        int row = Convert.ToInt32(Math.Floor(((mouse.Y - UtilityFunctions.FIELD_TOP) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP))));
+        int col = Convert.ToInt32(Math.Floor(((mouse.X - UtilityFunctions.FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP))));
 
-		if (SwinGame.MouseClicked(MouseButton.LeftButton)) {
-			int i = 0;
-			for (i = 0; i <= _menuStructure[menu].Length - 1; i++) {
-				//IsMouseOver the i'th button of the menu
-				if (IsMouseOverMenu(i, level, xOffset)) {
-					PerformMenuAction(menu, i);
-					return true;
-				}
-			}
+        if (row >= 0 && row < GameController.HumanPlayer.PlayerGrid.Height)
+        {
+            if (col >= 0 && col < GameController.HumanPlayer.PlayerGrid.Width)
+            {
+                // If in the area try to deploy
+                try
+                {
+                    GameController.HumanPlayer.PlayerGrid.MoveShip(row, col, _selectedShip, _currentDirection);
+                    UtilityFunctions.Message = "";
+                }
+                catch (Exception ex)
+                {
+                    Audio.PlaySoundEffect(GameResources.GameSound("Error"));
+                    UtilityFunctions.Message = ex.Message;
+                }
+            }
+        }
+    }
 
-			if (level > 0) {
-				//none clicked - so end this sub menu
-				GameController.EndCurrentState();
-			}
-		}
 
-		return false;
-	}
+    // '' <summary>
+    // '' Draws the deployment screen showing the field and the ships that the player can deploy.
+    // '' </summary>
+    public static void DrawDeployment()
+    {
+        UtilityFunctions.DrawField(GameController.HumanPlayer.PlayerGrid, GameController.HumanPlayer, true);
 
-	/// <summary>
-	/// Draws the main menu to the screen.
-	/// </summary>
-	public static void DrawMainMenu()
-	{
-		//Clears the Screen to Black
-		//SwinGame.DrawText("Main Menu", Color.White, GameFont("ArialLarge"), 50, 50)
+        // Draw the Left/Right and Up/Down buttons
+        if ((_currentDirection == Direction.LeftRight))
+        {
+            // the button for ext
+            SwinGame.DrawBitmap(GameResources.GameImage("LeftRightButton"), LEFT_RIGHT_BUTTON_LEFT, TOP_BUTTONS_TOP);
+        }
+        else
+        {
+            SwinGame.DrawBitmap(GameResources.GameImage("UpDownButton"), LEFT_RIGHT_BUTTON_LEFT, TOP_BUTTONS_TOP);
+        }
 
-		DrawButtons(MAIN_MENU);
-	}
+        // Draw ships depend on the selection
+        foreach (ShipName sn in Enum.GetValues(typeof(ShipName)))
+        {
+            int i = (int)(sn) - 1;
 
-	/// <summary>
-	/// Draws the Game menu to the screen
-	/// </summary>
-	public static void DrawGameMenu()
-	{
-		//Clears the Screen to Black
-		//SwinGame.DrawText("Paused", Color.White, GameFont("ArialLarge"), 50, 50)
+            if (i >= 0)
+            {
+                if ((sn == _selectedShip))
+                {
+                    SwinGame.DrawBitmap(GameResources.GameImage("SelectedShip"), SHIPS_LEFT, (SHIPS_TOP + (i * SHIPS_HEIGHT)));
+                }
 
-		DrawButtons(GAME_MENU);
-	}
+                SwinGame.DrawRectangle(Color.Blue, SHIPS_LEFT, SHIPS_TOP + i * SHIPS_HEIGHT, SHIPS_WIDTH, SHIPS_HEIGHT);
+                SwinGame.DrawText(sn.ToString(), Color.White, GameResources.GameFont("Courier"), SHIPS_LEFT + TEXT_OFFSET, SHIPS_TOP + i * SHIPS_HEIGHT);
+            }
 
-	/// <summary>
-	/// Draws the settings menu to the screen.
-	/// </summary>
-	/// <remarks>
-	/// Also shows the main menu
-	/// </remarks>
-	public static void DrawSettings()
-	{
-		//Clears the Screen to Black
-		//SwinGame.DrawText("Settings", Color.White, GameFont("ArialLarge"), 50, 50)
+        }
 
-		DrawButtons(MAIN_MENU);
-		DrawButtons(SETUP_MENU, 1, 1);
-	}
+        if (GameController.HumanPlayer.ReadyToDeploy)
+        {
+            SwinGame.DrawBitmap(GameResources.GameImage("PlayButton"), PLAY_BUTTON_LEFT, TOP_BUTTONS_TOP);
+        }
 
-	/// <summary>
-	/// Draw the buttons associated with a top level menu.
-	/// </summary>
-	/// <param name="menu">the index of the menu to draw</param>
-	private static void DrawButtons(int menu)
-	{
-		DrawButtons(menu, 0, 0);
-	}
+        SwinGame.DrawBitmap(GameResources.GameImage("RandomButton"), RANDOM_BUTTON_LEFT, TOP_BUTTONS_TOP);
+        UtilityFunctions.DrawMessage();
+    }
 
-	/// <summary>
-	/// Draws the menu at the indicated level.
-	/// </summary>
-	/// <param name="menu">the menu to draw</param>
-	/// <param name="level">the level (height) of the menu</param>
-	/// <param name="xOffset">the offset of the menu</param>
-	/// <remarks>
-	/// The menu text comes from the _menuStructure field. The level indicates the height
-	/// of the menu, to enable sub menus. The xOffset repositions the menu horizontally
-	/// to allow the submenus to be positioned correctly.
-	/// </remarks>
-	private static void DrawButtons(int menu, int level, int xOffset)
-	{
-		int btnTop = 0;
+    // '' <summary>
+    // '' Gets the ship that the mouse is currently over in the selection panel.
+    // '' </summary>
+    // '' <returns>The ship selected or none</returns>
+    private static ShipName GetShipMouseIsOver()
+    {
+        foreach (ShipName sn in Enum.GetValues(typeof(ShipName)))
+        {
+            int i = (int)(sn) - 1;
+            if (UtilityFunctions.IsMouseInRectangle(SHIPS_LEFT, (SHIPS_TOP + (i * SHIPS_HEIGHT)), SHIPS_WIDTH, SHIPS_HEIGHT))
+            {
+                return sn;
+            }
+        }
 
-		btnTop = MENU_TOP - (MENU_GAP + BUTTON_HEIGHT) * level;
-		int i = 0;
-		for (i = 0; i <= _menuStructure[menu].Length - 1; i++) {
-			int btnLeft = 0;
-			btnLeft = MENU_LEFT + BUTTON_SEP * (i + xOffset);
-			//SwinGame.FillRectangle(Color.White, btnLeft, btnTop, BUTTON_WIDTH, BUTTON_HEIGHT)
-			SwinGame.DrawTextLines(_menuStructure[menu][i], MENU_COLOR, Color.Black, GameResources.GameFont("Menu"), FontAlignment.AlignCenter, btnLeft + TEXT_OFFSET, btnTop + TEXT_OFFSET, BUTTON_WIDTH, BUTTON_HEIGHT);
-
-			if (SwinGame.MouseDown(MouseButton.LeftButton) & IsMouseOverMenu(i, level, xOffset)) {
-				SwinGame.DrawRectangle(HIGHLIGHT_COLOR, btnLeft, btnTop, BUTTON_WIDTH, BUTTON_HEIGHT);
-			}
-		}
-	}
-
-	/// <summary>
-	/// Determined if the mouse is over one of the button in the main menu.
-	/// </summary>
-	/// <param name="button">the index of the button to check</param>
-	/// <returns>true if the mouse is over that button</returns>
-	private static bool IsMouseOverButton(int button)
-	{
-		return IsMouseOverMenu(button, 0, 0);
-	}
-
-	/// <summary>
-	/// Checks if the mouse is over one of the buttons in a menu.
-	/// </summary>
-	/// <param name="button">the index of the button to check</param>
-	/// <param name="level">the level of the menu</param>
-	/// <param name="xOffset">the xOffset of the menu</param>
-	/// <returns>true if the mouse is over the button</returns>
-	private static bool IsMouseOverMenu(int button, int level, int xOffset)
-	{
-		int btnTop = MENU_TOP - (MENU_GAP + BUTTON_HEIGHT) * level;
-		int btnLeft = MENU_LEFT + BUTTON_SEP * (button + xOffset);
-
-		return UtilityFunctions.IsMouseInRectangle(btnLeft, btnTop, BUTTON_WIDTH, BUTTON_HEIGHT);
-	}
-
-	/// <summary>
-	/// A button has been clicked, perform the associated action.
-	/// </summary>
-	/// <param name="menu">the menu that has been clicked</param>
-	/// <param name="button">the index of the button that was clicked</param>
-	private static void PerformMenuAction(int menu, int button)
-	{
-		switch (menu) {
-			case MAIN_MENU:
-				PerformMainMenuAction(button);
-				break;
-			case SETUP_MENU:
-				PerformSetupMenuAction(button);
-				break;
-			case GAME_MENU:
-				PerformGameMenuAction(button);
-				break;
-		}
-	}
-
-	/// <summary>
-	/// The main menu was clicked, perform the button's action.
-	/// </summary>
-	/// <param name="button">the button pressed</param>
-	private static void PerformMainMenuAction(int button)
-	{
-		switch (button) {
-			case MAIN_MENU_PLAY_BUTTON:
-				GameController.StartGame();
-				break;
-			case MAIN_MENU_SETUP_BUTTON:
-				GameController.AddNewState(GameState.AlteringSettings);
-				break;
-			case MAIN_MENU_TOP_SCORES_BUTTON:
-				GameController.AddNewState(GameState.ViewingHighScores);
-				break;
-			case MAIN_MENU_QUIT_BUTTON:
-				GameController.EndCurrentState();
-				break;
-		}
-	}
-
-	/// <summary>
-	/// The setup menu was clicked, perform the button's action.
-	/// </summary>
-	/// <param name="button">the button pressed</param>
-	private static void PerformSetupMenuAction(int button)
-	{
-		switch (button) {
-			case SETUP_MENU_EASY_BUTTON:
-				GameController.SetDifficulty(AIOption.Easy);
-				break;
-			case SETUP_MENU_MEDIUM_BUTTON:
-				GameController.SetDifficulty(AIOption.Medium);
-				break;
-			case SETUP_MENU_HARD_BUTTON:
-				GameController.SetDifficulty(AIOption.Hard);
-				break;
-		}
-		//Always end state - handles exit button as well
-		GameController.EndCurrentState();
-	}
-
-	/// <summary>
-	/// The game menu was clicked, perform the button's action.
-	/// </summary>
-	/// <param name="button">the button pressed</param>
-	private static void PerformGameMenuAction(int button)
-	{
-		switch (button) {
-			case GAME_MENU_RETURN_BUTTON:
-				GameController.EndCurrentState();
-				break;
-			case GAME_MENU_SURRENDER_BUTTON:
-				GameController.EndCurrentState();
-				//end game menu
-				GameController.EndCurrentState();
-				//end game
-				break;
-			case GAME_MENU_QUIT_BUTTON:
-				GameController.AddNewState(GameState.Quitting);
-				break;
-		}
-	}
+        return ShipName.None;
+    }
 }
